75cddfcffffa74aad7701dab54e517c3e5338150
David Whitaker <damwhit@gmail.com>
2016-06-06 10:29:53 -0600

    write test for authentication

---
+++ spec/integration/user_can_log_in_and_out_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+RSpec.feature 'user creates an account' do
+  scenario 'they successfully make an account' do
+    visit '/'
+
+    fill_in 'email', with: 'david@gmail.com'
+    fill_in 'password', with: 'password'
+    fill_in 'password_confirmation', with: 'password'
+
+    click_on 'login'
+
+    expect(page).to have_content("Welcome david@gmail.com")
+  end
+end
