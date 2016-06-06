2b9641016fccc305924616485904bed2ef567f16
David Whitaker <damwhit@gmail.com>
2016-06-06 11:09:11 -0600

    user can log in test is passing

--- spec/integration/user_can_log_in_and_out_spec.rb
+++ spec/integration/user_can_log_in_and_out_spec.rb
@@ -3,9 +3,9 @@ RSpec.feature 'user creates an account' do
   scenario 'they successfully make an account' do
     visit '/'
 
-    fill_in 'email', with: 'david@gmail.com'
-    fill_in 'password', with: 'password'
-    fill_in 'password_confirmation', with: 'password'
+    fill_in 'user_email', with: 'david@gmail.com'
+    fill_in 'user_password', with: 'password'
+    fill_in 'user_password_confirmation', with: 'password'
 
     click_on 'signup'
 
