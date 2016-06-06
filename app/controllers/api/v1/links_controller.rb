da092ed689fb9af0d076e7cb4fa9cdf3a43c5fa6
David Whitaker <damwhit@gmail.com>
2016-06-06 16:47:16 -0600

    user can sort alphabetically by name

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -2,14 +2,13 @@ module Api
   module V1
     class LinksController < ApiController
       def index
-        require "pry"; binding.pry
         if params[:search]
           term = params[:search].try(:downcase)
           respond_with Link.where("LOWER(name) LIKE :term OR LOWER(url) LIKE :term", term: "%#{term}%")
         elsif params[:status]
           respond_with Link.where(read: params[:status])
-        elsif params[:status]
-          respond_with Link.where(created_at: :desc)
+        elsif params[:sort] == 'true'
+          respond_with Link.order(name: :asc)
         end
       end
 
